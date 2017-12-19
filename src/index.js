cc366712572a6be01bc72987d27c0b16c94bc546
采东 <fusheng.sfs@antfin.com>
2017-12-19 18:53:32 +0800

    暴露mesh

--- src/index.js
+++ src/index.js
@@ -14,19 +14,21 @@
  * @namespace Tiny.DragonBones
  * @memberof Tiny
  */
-import {default as dragonBones} from '../libs/dragonBones';
-const {BaseObject, BaseFactory, WorldClock, Armature, Animation, AnimationState, Bone} = dragonBones;
+import { default as dragonBones } from '../libs/dragonBones';
+const { BaseObject, BaseFactory, WorldClock, Armature, Animation, AnimationState, Bone } = dragonBones;
 
-import {default as TinyArmatureDisplay} from './TinyArmatureDisplay';
-import {default as TinyTextureAtlasData} from './TinyTextureAtlasData';
-import {default as TinyTextureData} from './TinyTextureData';
-import {default as TinySlot} from './TinySlot';
-import {default as DisplayType} from './DisplayType';
-import {default as BlendMode} from './BlendMode';
-import {default as BoundingBoxType} from './BoundingBoxType';
-import {default as TinyFactory} from './TinyFactory';
+import { default as TinyArmatureDisplay } from './TinyArmatureDisplay';
+import { default as TinyTextureAtlasData } from './TinyTextureAtlasData';
+import { default as TinyTextureData } from './TinyTextureData';
+import { default as TinySlot } from './TinySlot';
+import { default as DisplayType } from './DisplayType';
+import { default as BlendMode } from './BlendMode';
+import { default as BoundingBoxType } from './BoundingBoxType';
+import { default as TinyFactory } from './TinyFactory';
 
-import {addDragonBonesData, addTextureAtlasData, buildArmatureDisplay} from './GloabalMethods';
+import { addDragonBonesData, addTextureAtlasData, buildArmatureDisplay } from './GloabalMethods';
+
+import { Mesh } from 'tinyjs-plugin-mesh';
 
 export {
   TinyArmatureDisplay,
@@ -49,4 +51,6 @@ export {
   Armature,
   Animation,
   AnimationState,
+
+  Mesh,
 };
